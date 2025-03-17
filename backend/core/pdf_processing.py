update: optimize for view and upload pdf file, pdf2image instead of fitz, fix rotation image bug

backend/core/pdf_processing.py
@@ -1,18 +1,15 @@
 import os
 import re
 import unicodedata
-import fitz
 import cv2 as cv
 import numpy as np
 from io import BytesIO
 import requests
 from fastapi import HTTPException
 from minio.error import S3Error
-import pdfplumber
-from pdfminer.pdfparser import PDFSyntaxError
-import hashlib
+from pdf2image import convert_from_bytes
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from config import MINIO_BUCKET, IMAGE_MINIO_BUCKET, QDRANT_COLLECTION, minio_client, image_minio_client, vectordb_provider
+from service_config import MINIO_BUCKET, QDRANT_COLLECTION, minio_client, vectordb_provider
 from fastapi import HTTPException, UploadFile
 
 
@@ -25,25 +22,6 @@ def normalize_filename(filename: str) -> str:
     ascii_str = nfkd_form.encode("ASCII", "ignore").decode("ASCII")
     ascii_str = ascii_str.replace(" ", "_")
     return re.sub(r"[^\w\-.]", "", ascii_str)
-
-def normalize_filename(filename: str) -> str:
-    base = os.path.basename(filename)
-    nfkd_form = unicodedata.normalize('NFKD', base)
-    ascii_str = nfkd_form.encode('ASCII', 'ignore').decode('ASCII')
-    ascii_str = ascii_str.replace(" ", "_")
-    return re.sub(r'[^\w\-.]', '', ascii_str)
-
-def rotate_image_by_angle(image, angle):
-    (h, w) = image.shape[:2]
-    (cX, cY) = (w // 2, h // 2)
-    M = cv.getRotationMatrix2D((cX, cY), -angle, 1.0)
-    cos = np.abs(M[0, 0])
-    sin = np.abs(M[0, 1])
-    nW = int((h * sin) + (w * cos))
-    nH = int((h * cos) + (w * sin))
-    M[0, 2] += (nW / 2) - cX
-    M[1, 2] += (nH / 2) - cY
-    return cv.warpAffine(image, M, (nW, nH), flags=cv.INTER_CUBIC, borderMode=cv.BORDER_REPLICATE)
 
 def check_and_upload_minio(uploaded_file: UploadFile):
     normalized_filename = normalize_filename(uploaded_file.filename)
@@ -72,44 +50,15 @@ def check_and_upload_minio(uploaded_file: UploadFile):
         else:
             raise HTTPException(status_code=500, detail=f"File upload error: {err}")
 
-def render_full_page(page, dpi=500):
-    # Render full page image
-    pix = page.get_pixmap(dpi=dpi)
-    image_bytes = pix.tobytes("jpg")
-    image_array = cv.imdecode(np.frombuffer(image_bytes, dtype=np.uint8), cv.IMREAD_COLOR)
-    return image_array
-
-def process_embedded_images(doc, page, pdf_file_name, page_index):
-    images = page.get_images(full=True)
-    for img in images:
-        xref = img[0]
-        base_image = doc.extract_image(xref)
-        embedded_bytes = base_image.get("image")
-        image_array = cv.imdecode(np.frombuffer(embedded_bytes, dtype=np.uint8), cv.IMREAD_COLOR)
-        yield image_array 
-
-
 def process_image(image_array, pdf_file_name, page_index):
     if image_array is None:
         raise ValueError(f"Can not decode pdf image page: {page_index+1}")
+    
     # Encode image -> JPEG
     success, encoded_image = cv.imencode(".jpg", image_array, [int(cv.IMWRITE_JPEG_QUALITY), 95])
     if not success:
         raise ValueError(f"Can not encode pdf image page: {page_index+1}")
     jpeg_bytes = encoded_image.tobytes()
-    
-    # Save image in minio 
-    image_filename = f"{pdf_file_name}_page{page_index+1}.jpg"
-    image_stream = BytesIO(jpeg_bytes)
-    file_size = len(jpeg_bytes)
-    image_minio_client.put_object(
-        IMAGE_MINIO_BUCKET,
-        image_filename,
-        image_stream,
-        file_size,
-        content_type="image/jpeg"
-    )
-    print(f"Image {image_filename} uploaded to minio.")
     
     files = {"image": (pdf_file_name, jpeg_bytes, "image/jpeg")}
     response = requests.post(EXTRACT_TEXTS_URL_PYTESSERACT, files=files)
@@ -120,50 +69,12 @@ def process_image(image_array, pdf_file_name, page_index):
 
 def process_pdf_and_store(pdf_bytes, pdf_file_name):
 
-    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
-    
-    for page_index, page in enumerate(doc):
-
-        rotation = page.rotation if page.rotation else 0
-        
-        # Render full page image
-        full_page_image = render_full_page(page, dpi=500)
-        if rotation:
-            full_page_image = rotate_image_by_angle(full_page_image, rotation)
-        process_image(full_page_image, pdf_file_name, page_index)  
-        
-        # Process embedded images
-        for embedded_image in process_embedded_images(doc, page, pdf_file_name, page_index):
-            if rotation:
-                embedded_image = rotate_image_by_angle(embedded_image, rotation)
-            process_image(embedded_image, pdf_file_name, page_index)  
-    return {"status": "success", "message": "All pages processed and images stored."}
-
-pdf_cache = {}
-def get_pdf_cache_key(file_bytes: bytes, dpi: int) -> str:
-    return hashlib.sha256(file_bytes + str(dpi).encode()).hexdigest()
-
-def extract_all_pages_images_cached(file_bytes, dpi=150):
-    key = get_pdf_cache_key(file_bytes, dpi)
-    if key in pdf_cache:
-        return pdf_cache[key]
-    
-    images = extract_all_pages_images(file_bytes, dpi=dpi)
-    pdf_cache[key] = images
-    return images
-
-def extract_all_pages_images(file_bytes, dpi=300):
     try:
-        file_obj = BytesIO(file_bytes)
-        file_obj.seek(0)
-        with pdfplumber.open(file_obj) as pdf:
-            images = []
-            for page in pdf.pages:
-                pil_image = page.to_image(resolution=dpi).original
-                image_array = cv.cvtColor(np.array(pil_image), cv.COLOR_RGB2BGR)
-                images.append(image_array)
-            return images
-    except PDFSyntaxError:
-        raise HTTPException(status_code=400, detail="This file is not a valid PDF or it is corrupted.")
+        images = convert_from_bytes(pdf_bytes, dpi=300)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error occurred while processing PDF: {e}")
+        raise HTTPException(status_code=400, detail=f"Error converting PDF to images: {e}")
+    
+    for page_index, pil_image in enumerate(images):
+        image_array = cv.cvtColor(np.array(pil_image), cv.COLOR_RGB2BGR)
+        process_image(image_array, pdf_file_name, page_index)
+    return {"status": "success", "message": "All pages processed and images stored."}

backend/core/routes/pdf_routes.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, HTTPException, UploadFile, File, Depends
 from auth import get_current_user
-from pdf_processing import check_and_upload_minio, process_pdf_and_store, extract_all_pages_images_cached, normalize_filename
+from pdf_processing import check_and_upload_minio, process_pdf_and_store, normalize_filename
 from minio.error import S3Error
-from config import MINIO_BUCKET, QDRANT_COLLECTION, minio_client, vectordb_provider
-from typing import Optional, List
+from service_config import MINIO_BUCKET, QDRANT_COLLECTION, minio_client, vectordb_provider
+from typing import List
 import base64
 import cv2 as cv
+import numpy as np 
+from pdf2image import convert_from_bytes
 
 router = APIRouter()
 
@@ -48,26 +50,25 @@ def list_pdfs(current_user=Depends(get_current_user)):
 
 
 @router.get("/pdf/view/{filename}")
-def view_pdf(filename: str, page: Optional[int] = None, dpi: int = 150, user=Depends(get_current_user)):
+def view_pdf(filename: str, page: int = 0, dpi: int = 300, user=Depends(get_current_user)):
     try:
         response = minio_client.get_object(MINIO_BUCKET, filename)
         file_bytes = response.read()
-        images = extract_all_pages_images_cached(file_bytes, dpi=dpi)
+        # convert pdf page to image
+        images = convert_from_bytes(file_bytes, dpi=dpi)
+        total_pages = len(images)
+        if page < 0 or page >= total_pages:
+            raise HTTPException(status_code=400, detail=f"Invalid page number. Total pages: {total_pages}")
+        # Choosen page
+        pil_image = images[page]
+
+        image_array = cv.cvtColor(np.array(pil_image), cv.COLOR_RGB2BGR)
+        success, buffer = cv.imencode('.jpg', image_array)
         
-        # Lazy loading
-        if page is not None:
-            if page < 0 or page >= len(images):
-                raise HTTPException(status_code=400, detail="Invalid page number")
-            images = [images[page]]
+        if not success:
+            raise HTTPException(status_code=500, detail="Error encoding image.")
+        img_base64 = base64.b64encode(buffer).decode('utf-8')
         
-        images_base64 = []
-        for img in images:
-            success, buffer = cv.imencode('.jpg', img)
-            if not success:
-                continue
-            img_base64 = base64.b64encode(buffer).decode('utf-8')
-            images_base64.append(img_base64)
-            
-        return {"filename": filename, "images": images_base64}
+        return {"filename": filename, "page": page, "total_pages": total_pages, "image": img_base64}
     except Exception as e:
-        raise HTTPException(status_code=404, detail=f"File not found: {filename}")
+        raise HTTPException(status_code=404, detail=f"File not found or error processing PDF: {e}")